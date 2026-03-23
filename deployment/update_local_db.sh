0c3369de15e32d4c17ae76beade45149ad8049ef
Peter <pjmakey2@gmail.com>
2026-03-22 22:18:25 -0300

    - Actualizacion de la base local desde la produccion

---
+++ deployment/update_local_db.sh
@@ -0,0 +1,7 @@
+python manage.py fl_legacy_mainline --sync_remote \
+      --src_host 165.227.23.149 \
+      --src_user upj_sys \
+      --src_pass 'ZrLn4j0@Yo*l1fyu' \
+      --dst_host 143.110.238.24 \
+      --dst_user fesifen \
+      --dst_pass 'Db#9Xv!27LmQ@t5RzK4pHs'
