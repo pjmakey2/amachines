b717434ab74e5dee8c7a8d2006fad3f706232859
Peter <pjmakey2@gmail.com>
2026-07-30 11:49:52 -0300

    - Crontab

--- Sifen/bin/mng_sifen_send_pending.sh
+++ Sifen/bin/mng_sifen_send_pending.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Send pending documents to Sifen
 # This script is meant to run via cron every 5 minutes
-
-cd /app
-python manage.py mng_sifen_mainline --send_pending_docs
+PYTHON=/home/am/.pyenv/versions/Amachine/bin/python
+MANAGE=/home/am/projects/Amachine/manage.py
+$PYTHON $MANAGE mng_sifen_mainline --send_pending_docs
